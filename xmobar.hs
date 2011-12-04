0422b72e9a371ee203928f86874b3f945c10e9e8
Jan <jangingnicht@web.de>
2011-12-04 20:14:25 +0100

    changed xmobar to terminus font

--- xmobar.hs
+++ xmobar.hs
@@ -2,7 +2,7 @@
 -- based on the config of Vic Fryzel
 
 Config {
-    font = "xft:Fixed-8",
+    font = "xft:Terminus:antialias=true:pixelsize=10",
     bgColor = "#000000",
     fgColor = "#ffffff",
     position = Static { xpos = 0, ypos = 0, width = 1320, height = 16 },
@@ -12,8 +12,8 @@ Config {
         Run MultiCpu ["-t","Cpu: <total0> <total1>","-L","3","-H","50","--normal","#CEFFAC","--high","#FFB6B0","-w","3"] 10,
         Run Memory ["-t","Mem: <usedratio>%"] 10,
         Run Swap [] 10,
-        Run Network "eth0" ["-t","eth0: <rx>, <tx>","-H","1000","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 5,
-        Run Network "wlan0" ["-t","wlan0: <rx>, <tx>","-H","1000","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 5,
+        Run Network "eth0" ["-t","eth0: <rx>, <tx>","-H","1000","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 9,
+        Run Network "wlan0" ["-t","wlan0: <rx>, <tx>","-H","1000","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 9,
         Run Date "%a %_d %b %k:%M" "date" 10,
         Run StdinReader
     ],
